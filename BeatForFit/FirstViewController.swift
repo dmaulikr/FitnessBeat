00792e5f6e887498d12e3084c452096d54465b2d
Grigory Bochkarev <g.bochkaryov@innopolis.ru>
2016-06-21 14:14:41 +0300

    Initial Commit

---
+++ BeatForFit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BeatForFit
+//
+//  Created by Grigory Bochkarev on 21.06.16.
+//  Copyright © 2016 Grigory Bochkarev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
